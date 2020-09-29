b51b97f5ff6daad18a1911780c73d6bc3c4107a4
kranthi pyreddy <kra330@gmail.com>
2020-09-29 13:53:46 -0400

    Added a New Class and created Instances and method to call from main method

--- src/map103lab2/MAP103Lab2.java
+++ src/map103lab2/MAP103Lab2.java
@@ -1,10 +1,33 @@
 package map103lab2;
 
+import java.util.Scanner;
+
 public class MAP103Lab2 {
 
-    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create a Scanner to obtain information from the user
+        Scanner input = new Scanner(System.in);
+// Pulls a random number between 0 and 4
+        int questionNumber = (int) (Math.random() * 5);
+        RandomQuizHelper helper = new RandomQuizHelper();
+// Uses a method in class RandomQuizHelper that takes
+// a question number and returns the question.
+        String question = helper.getQuestion(questionNumber);
+// A variable to store the users answer
+        String answer = "";
+        
+        System.out.print(question);
+        
+        answer = input.nextLine();
+        
+        if (helper.getResult(questionNumber, answer) == true) {
+            System.out.print("You are correct!");
+        } else {
+            System.out.print("You are not correct.");
+        }
+        input.close();
     }
-    
 }
+
+    
+
